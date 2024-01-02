3eaef230ce36897788dee012bd1d75c38ca12e89
Javi H. Gil <javihgil@softspring.es>
2024-01-02 13:52:32 +0100

    Remove unused bind variable

---
+++ src/Form/EntityCreateFormInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Softspring\Component\CrudlController\Form;
+
+use Symfony\Component\Form\FormTypeInterface;
+
+/**
+ * @deprecated since 5.1, will be removed in 6.0
+ */
+interface EntityCreateFormInterface extends FormTypeInterface
+{
+}
